a75c5b9bf399179f8ca6d146b412f49c03d149da
sAMs126 <492457689@qq.com>
2020-02-04 15:25:42 +0800

    :bulb: 开发 carousel 组件

--- ng-action-one/src/app/carousel/carousel.component.ts
+++ ng-action-one/src/app/carousel/carousel.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import Swiper from "swiper";
 
 @Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  selector: "app-carousel",
+  templateUrl: "./carousel.component.html",
+  styleUrls: ["./carousel.component.css"]
 })
 export class CarouselComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    var swiper = new Swiper(".swiper-container", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      }
+    });
   }
-
 }
