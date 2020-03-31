f60434487ff05528c0fbdeb08d4f29bc1385631a
sAMs126 <492457689@qq.com>
2020-03-31 18:14:08 +0800

    :speech_balloon: Observer 观察者三种通知类型

--- ng-observer/src/app/component/bind/bind.component.ts
+++ ng-observer/src/app/component/bind/bind.component.ts
@@ -14,8 +14,11 @@ export class BindComponent implements OnInit {
       .filter(e => e % 2 === 0)
       .map(e => e * e)
       .subscribe(
+        // 处理流中发射出来的元素
         e => console.log(e),
+        // 处理流中的异常
         err => console.error(err),
+        // 在流结束时调用
         () => console.log("结束")
       )
   }
