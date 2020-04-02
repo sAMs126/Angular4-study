05df77473cd73bc2d599821b2b359c15228b15ba
sAMs126 <492457689@qq.com>
2020-04-02 19:27:47 +0800

    :bulb: 输入属性

--- ng-interaction/src/app/app.module.ts
+++ ng-interaction/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ChildComponent } from './component/child/child.component';
+import { ParentComponent } from './component/parent/parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    ParentComponent
   ],
   imports: [
     BrowserModule,
---
+++ ng-interaction/src/app/component/child/child.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  @Input()
+  stockCode: string;
+
+  @Input()
+  amount: number;
+
+  constructor () { }
+
+  ngOnInit() {
+  }
+
+}
