f999eade9e2e8aaf07c95af9684dfd0cf0057a45
sAMs126 <492457689@qq.com>
2020-04-10 15:35:32 +0800

    :bulb: ngDoCheck 变更检测机制

--- ng-lifecycle/src/app/component/child/child.component.ts
+++ ng-lifecycle/src/app/component/child/child.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit, OnChanges {
+export class ChildComponent implements OnInit, OnChanges, DoCheck {
 
   @Input()
   greeting: string;
@@ -15,12 +15,37 @@ export class ChildComponent implements OnInit, OnChanges {
 
   message: string = "初始化消息";
 
+  oldUserNameTemp: string;
+
+  changeDetected: boolean = false;
+
+  noChangeCount: number = 0;
+
   constructor () { }
 
   ngOnInit() {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. 
+    //Use it to inject dependencies, but avoid any serious work here.
     console.log(JSON.stringify(changes, null, 2));
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked.
+    //Use it to extend change detection by performing a custom check.
+    if (this.user.name !== this.oldUserNameTemp) {
+      this.changeDetected = true;
+      console.log("DoCheck: user.name 从\t" + this.oldUserNameTemp + "\t变更为\t" + this.user.name);
+      this.oldUserNameTemp = this.user.name;
+    }
+    if (this.changeDetected) {
+      this.noChangeCount = 0;
+    } else {
+      this.noChangeCount++;
+      console.log("DoCheck: user.name 未发生改变，ngDoCheck 方法被调用\t" + this.noChangeCount + "\t次");
+    }
+    this.changeDetected = false;
   }
 }
