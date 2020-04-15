59a175e89e94b05ddd9df7c2848a40cb1b3d7405
sAMs126 <492457689@qq.com>
2020-04-15 21:04:27 +0800

    :bulb:  响应式表单中进行表单验证

--- ng-forms/src/app/reactive-regist-form/reactive-regist-form.component.ts
+++ ng-forms/src/app/reactive-regist-form/reactive-regist-form.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, FormBuilder, AbstractControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-reactive-regist-form',
-  templateUrl: './reactive-regist-form.component.html',
-  styleUrls: ['./reactive-regist-form.component.css']
+  selector: "app-reactive-regist-form",
+  templateUrl: "./reactive-regist-form.component.html",
+  styleUrls: ["./reactive-regist-form.component.css"]
 })
 export class ReactiveRegistFormComponent implements OnInit {
 
@@ -12,21 +12,42 @@ export class ReactiveRegistFormComponent implements OnInit {
 
   constructor (private fb: FormBuilder) {
     this.formModel = this.fb.group({
-      username: [''],
-      phone: [''],
-      area: [''],
-      pwdGroup: this.fb.group({
-        password: [''],
-        pconfirm: ['']
-      })
+      username: ["", [Validators.required, Validators.minLength(6)]],
+      phone: ["", this.phoneValidator],
+      area: [""],
+      pwdGroup: this.fb.group(
+        {
+          password: [""],
+          pconfirm: [""]
+        },
+        { validator: this.pwdEqualValidator }
+      )
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   onSubmit() {
+    let isValid = this.formModel.get("username").valid;
+    console.log("username 的校验结果：" + isValid);
+    let errors = this.formModel.get("username").errors;
+    console.log("username 的错误信息：\n" + JSON.stringify(errors, null, 2));
     console.log(this.formModel.value);
   }
 
+  // 自定义校验器 返回结果： { [key: string]: any }
+  phoneValidator(control: FormControl): any {
+    var reg = /^(13[0-9]{1}|14[5|7]|(15[0-9]{1})|(18[0-9]{1}))+\d{8}$/;
+    let valid = reg.test(control.value);
+    console.log("phone 的校验结果：" + valid);
+    return valid ? null : { phone: true };
+  }
+
+  pwdEqualValidator(gruop: FormGroup): any {
+    let password: FormControl = gruop.get("password") as FormControl;
+    let pconfirm: FormControl = gruop.get("pconfirm") as FormControl;
+    let valid = password.value === pconfirm.value;
+    console.log("password 的校验结果：" + valid);
+    return valid ? null : { equal: true };
+  }
 }
